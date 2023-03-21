eefe1c99d45027f3f50170de356bbcb136efb1f6
Lau-na <luanetesta2@gmail.com>
2023-03-21 20:06:03 -0300

    Atividade finalizada

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
-import ThemeContext from './context/ThemeContext';
+import React, { useState } from "react";
+import ThemeContext from "./context/ThemeContext";
 import Header from "./components/Header";
 import ThemeToggler from "./components/ThemeToggler";
 import Footer from "./components/Footer";
+
 export default function App() {
   const themeHook = useState("light");
   return (
